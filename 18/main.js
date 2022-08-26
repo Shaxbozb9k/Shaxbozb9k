d95ce6e99d4b5495bc4ec3bdd2ae18723032f15f
Shaxbozb9k <diiiishiiii@gmail.com>
2022-08-26 20:55:55 +0500

    This is programm close

---
+++ 18/main.js
@@ -0,0 +1,48 @@
+// let ==> bu oz`garuvchini o`zlashtirish uchun kalit so`zi
+
+
+// let message;
+// let number = 15;
+
+// let Shaxa = "(20+20)";
+// let shaxa = 20 ;
+
+// number o`zgaruvchi nomi
+
+// console.log(number+ 305 +shaxa) //"50+5" 
+
+//***********************************************************************Arximetika usuli *****************************************************************/ 
+
+
+let Con = 25;
+var B5 = 57 ;
+
+console.log(Con+ 305 +B5) 
+console.log(Con*B5) 
+console.log(Con+B5) 
+console.log(Con-B5) 
+console.log(Con%B5) 
+console.log(Con/B5)  
+
+let text = "Hello", text1 = "world";
+console.log(text+" "+text1);
+text = "Salom";
+console.log(text+" "+"Anvar")
+console.log(text*Number)
+console.debug(text+" "+text1);
+
+let son = "158.5", son1 = 78;
+console.log (parseInt(son)+son1);
+console.log (parseFloat(son)+son1);
+
+// console.log =(number2+parseFloat(number8));
+
+ let num = 23, num1 = 86;
+// console.log (num)
+// console.log (num1 - "11")
+
+// console.log (num1)
+// console.log(num)
+
+console.log ((num*num2)/num)
+console.log ((num2/num)*num1)
---
+++ 19/main.js
@@ -0,0 +1,318 @@
+// // "=" bu belgi teng degani
+// let archa = "Qarag`ay";
+// // "!" bu qa`tiy tengmas degani
+// // "===" qa`tiy teng degani 
+// // "==" bu tengmi ya`ni qa`tiy bolmagan tenglik
+// let son1 = 12, son2 = 26, son3 = "12";
+
+// console.log(son1 == son2);
+// console.log(son1 == son3);
+// console.log(son1 === son3);
+
+
+// // "!=" bu belgi teng emas 
+// // son1 != 6;
+
+// // ">" bu belgi katta
+// // "<" bu belgi kichik
+// // "<=" bu belgi kichik yoki teng
+// // ">=" bu belgi katta yoki teng
+
+// console.log(son1 != son2);
+// console.log(son1 != son3);
+// console.log(son1 !== son2);
+
+// // *****************************shart operatorlari***********************//
+// let numberOne = 22, numberTwo = 17;
+// if (numberOne > numberTwo) {
+//     console.log("Kattasi:" + numberOne)
+// } else if (numberOne > numberTwo) {
+//     console.log("Kattasi:" + numberTwo)
+// } else {
+//     console.log("Ikkalasi tend")
+// }
+
+// let vazin = 60;
+// if (vazin > 60) {
+//     console.log("Siz Semizsiz teziroq Ozing")
+// } else if (vazin < 60) {
+//     console.log("Xotirjam boling vaznis judayam yaxshi")
+// } else {
+//     console.log("Vaziz joyida")
+// }
+
+// let haftakunlari = 7;
+// if (haftakunlari == 1) {
+//     console.log("Dushanba")
+// } else if (haftakunlari === 2) {
+//     console.log("Sishanba")
+// } else if (haftakunlari === 3) {
+//     console.log("Chorshanba")
+// } else if (haftakunlari === 4) {
+//     console.log("Payshanba")
+// } else if (haftakunlari === 5) {
+//     console.log("Juma")
+// } else if (haftakunlari === 6) {
+//     console.log("Shanba")
+// } else if (haftakunlari === 7) {
+//     console.log("Yakshanba")
+// } else if (haftakunlari > 7) {
+//     console.log(haftakunlari + " " + "degan hafta kunlari yo`q")
+// }
+
+// let haftakunlari1 = 1;
+
+// switch (haftakunlari1) {
+//     case 1:
+//         console.log("Qish");
+//         break;
+//     case 2:
+//         console.log("Qish");
+//         break;
+//     case 3:
+//         console.log("Baxor");
+//         break;
+//     case 4:
+//         console.log("Baxor");
+//         break;
+//     case 5:
+//         console.log("Baxor");
+//         break;
+//     case 6:
+//         console.log("Yoz");
+//         break;
+//     case 7:
+//         console.log("Yoz");
+//         break;
+//     case 8:
+//         console.log("Yoz");
+//         break;
+//     case 9:
+//         console.log("Kuz");
+//         break;
+//     case 10:
+//         console.log("Kuz");
+//         break;
+//     case 11:
+//         console.log("Kuz");
+//         break;
+//     case 12:
+//         console.log("Qish");
+//         break;
+
+//     default:
+//         console.log(haftakunlari1 + "-Oy yoq");
+//         break;
+
+// }
+
+
+// let yosh = 13;
+// if (yosh <= 13) {
+//     console.log("Kichik")
+// } else if (yosh <= 18) {
+//     console.log("O'smir")
+// } else if (yosh <= 35) {
+//     console.log("O'rta yosh")
+// } else if (yosh <= 50) {
+//     console.log("Qari")
+// } else if (yosh <= 100) {
+//     console.log("Yoshis yetib kelibti!!!!!")
+// }
+
+
+
+// // let oylama = 5;
+// // console.log(oylama * "2");
+
+
+// let sovuq = 0;
+// switch (sovuq) {
+//     case 1:
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//     case 6:
+//     case 7:
+//     case 8:
+//     case 9:
+//     case 10:
+//     case 11:
+//     case 12:
+//     case 13:
+//     case 14:
+//     case 15:
+//     case 16:
+//     case 17:
+//     case 18:
+//     case 19:
+//     case 20:
+//         console.log("Konditsonerni ochir");
+//         break;
+//     case 21:
+//     case 22:
+//     case 23:
+//     case 24:
+//     case 25:
+//     case 26:
+//     case 27:
+//     case 28:
+//     case 29:
+//     case 30:
+//     case 31:
+//     case 32:
+//     case 33:
+//     case 34:
+//     case 35:
+//     case 36:
+//     case 37:
+//     case 38:
+//     case 39:
+//     case 40:
+//         console.log("Konditsonerni Yo`q");
+//         break;
+//     default:
+//         console.log("Bunday ma`lumotni topolmadim")
+// }
+
+// let 
+
+// && => va degan manoni bildiradi
+
+// T => True F =>False
+// T+T=T
+// T+F=F
+// F+T=F
+// F+F=F
+
+// || => yoki degan mano bildiradi
+// T+T=T
+// T+F=T
+// F+T=T
+// F+F=F
+// Expensiv function
+// function myFunction(ishkuni, tatil) {
+//     // block code
+//     if (ishkuni === false && tatil === false) {
+//         document.write(true) 
+//     } else if (ishkuni === true && tatil === false) {
+//         document.write(false)
+//     } else if (ishkuni === false && tatil === true) {
+//         document.write(true)
+//     }
+
+// }
+// console.log(myFunction(true , false))
+
+
+
+// // Declaration function
+
+// let a = function meningFuntion() {
+
+// }
+
+// let random = (a) => {
+
+// }
+
+// function myFunction (ishkuni,tatil){
+//     if(ishkuni && !tatil){
+//         console.log(true)
+//     }else if (ishkuni && !tatil){
+//         console.log(false)
+//     }else if (!ishkuni && tatil){
+//         console.log(true)
+//     }
+// }
+
+// console.log(myFunction(true, false))
+
+
+
+
+// function monkeyTrouble (aSmile,bSmile){
+//     if(aSmile && bSmile){
+//         console.log(true)
+//     } else if (!aSmile && !bSmile){
+//         console.log(true)
+//     } else if (aSmile && !bSmile || !aSmile && bSmile){
+//         console.log(false)
+//     }  
+// }
+// console.log(monkeyTrouble(false, true))
+
+
+//             function sumDouble (a , b){
+//                 if(a === b ){
+//                     console.log((a+b)*2)
+//                 } else if(a && b ){
+//                     console.log(a+b)
+//     }
+// }
+
+// console.log(sumDouble(2,2))
+
+
+
+// function parrotTrouble(talking, hour) {
+//     if (talking && (hour < 7 || hour > 20)) {
+//         return true
+//     } else if (!talking && (hour > 7 || hour < 20)) {
+//         return false
+//     }
+
+// }
+// console.log(parrotTrouble(false, 6))
+
+// function misolim(str) {
+
+//     if (str.length >= 3)
+//         front = str.substring(0, 3);
+//     else
+//         front = str;
+//     return (front + front + front);
+// }
+
+// console.log(misolim("code"))
+
+// function misolim(a, b, negative) {
+//     if (a > -1 && b < 0 && !negative || a < -1 && b > -1 && !negative) {
+//         return true
+//     } else {
+//         return false;
+//     }
+// }
+// console.log(misolim(-4, -5, true))
+
+
+function misolim (n){
+    if (Math.abs(100-n) <= 10 || Math.abs(200-n) <= 10){
+    return  true
+    } else {
+      return false;
+    }
+  }
+  console.log(misolim(199))
+  
+
+//   function misolim (n){ 
+// 	if (n <= 21){
+// 	return  21 - n;
+// 	} else {
+// 		return 2 * (n - 21);
+// 	}
+// }
+// console.log(misolim(19))
+
+// function misolim (a,b){
+// 	if (a < 10 && b >9 || a <= 1 && b >= 9){
+// 	return  true
+// 	} else if (a === b){
+// 		return false;
+// 	}
+// }
+// console.log(misolim(1,9))
+
+
